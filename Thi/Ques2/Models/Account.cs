ff9bd6af3aac670896c40184d77595569f93bfa1
datyasuo1 <73332200+datyasuo1@users.noreply.github.com>
2021-11-22 23:01:23 +0700

    up

---
+++ Thi/Ques2/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thi.Ques2.Models
+{
+    class Account
+    {
+        public string Username { get; set; }
+    }
+}
